19a5189240cc943c9cfbc3a48b51e9ec671ccb60
siddhartha lamichhane <siddharthalamichhane56@gmail.com>
2025-11-27 23:10:08 +0545

    creating models for the zoom

---
+++ backend/controllers/socketManager.js
@@ -0,0 +1,5 @@
+import { Server } from "socket.io";
+export const connectToSocket = (server) => {
+  const io = new Server(server);
+  return io;
+};
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,41 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.set("port", process.env.PORT || 3000);
+
 app.get("/", (req, res) => {
-  return res.json({
-    message: "hello from get server",
-  });
+  return res.json({ message: "hello from get server" });
 });
-app.listen(3000, () => {
-  console.log("server is running");
-});
+
+const port = app.get("port");
+
+// Start Server
+const start = async () => {
+  try {
+    const connectionDB = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB connected: ${connectionDB.connection.host}`);
+
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Mongo connection error:", error);
+    process.exit(1);
+  }
+};
+
+start();
